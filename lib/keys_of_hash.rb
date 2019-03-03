3e887b6b36c7c656630981b9ac91c02d1bd34e62
Robert Tate <btate712@gmail.com>
2019-03-03 22:01:37 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -2,14 +2,14 @@ require 'pry'
 
 class Hash
   def keys_of(*arguments)
-    binding.pry
     return_array = []
     arguments.each do |argument|
-      this.each do |key, value|
-        if value = argument
-          return_array.push(key)
-        end
-      end
+    #search a hash for values that match argument and add them to return_array
+    
+    self.each do |key, value|
+       if value == argument
+         return_array.push(key)
+       end
     end
   return_array
   end
