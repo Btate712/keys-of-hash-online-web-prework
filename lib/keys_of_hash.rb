9b793c3148a23b9e16745b92ca711473a9462630
Robert Tate <btate712@gmail.com>
2019-03-03 21:33:26 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,11 @@
 class Hash
   def keys_of(*arguments)
     return_array = []
+    arguments.each do |key, value|
+      if value = arguments
+        return_array.push(key)
+      end
+    end
+  return_array
   end
 end
