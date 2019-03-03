3ba23fd82f8309bbae2f9292d6522129298809d8
Robert Tate <btate712@gmail.com>
2019-03-03 22:01:40 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -5,7 +5,7 @@ class Hash
     return_array = []
     arguments.each do |argument|
     #search a hash for values that match argument and add them to return_array
-    
+
     self.each do |key, value|
        if value == argument
          return_array.push(key)
